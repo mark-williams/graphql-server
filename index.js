7739127b334eb47e89bb688d30c5abab7c3d5785
mark-williams <markwill@hotmail.co.uk>
2018-05-07 08:55:34 +0100

    Very (very!) simple graphql schema, query and resolver

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const a = 2;
+const { graphql, buildSchema } = require('graphql');
 
-let mica = a => {
-  return a;
+/* eslint-disable no-console */
+const schema = buildSchema(`
+  type Query {
+    foo: String
+  }
+
+  type Schema {
+    query: Query
+  }
+`);
+
+const resolvers = {
+  foo: () => 'bar'
 };
+
+const query = `
+  query myQuery {
+    foo
+  }
+`;
+
+graphql(schema, query, resolvers).then(x => console.log(x));
